143beb7d91b3e7bf0218cce24ad30e76132aa08a
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-09-03 11:27:39 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-center mt-5 text-2xl font-medium uppercase">
+        React Reducer Intro
+      </h1>
+    </div>
+  );
+}
+
+export default App;
